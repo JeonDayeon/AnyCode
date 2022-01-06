6fea0d30ab57c937a776776cb29b5c57d2cd0fa4
JeonDayeon <95409013+JeonDayeon@users.noreply.github.com>
2022-01-06 10:45:21 +0900

    Update 기말.c

--- 기말.c
+++ 기말.c
@@ -11,23 +11,24 @@ int main()
 
 void One()
 {
-	int j;
-	float num[30] =
-	{
-		1.2, 2.2, 3.3, 4.3, 5.3, 6.3, 7.3, 8.3, 9.3, 10.3,
-		11.3, 12.3, 13.3, 14.3, 15.3, 16.3, 17.3, 18.3, 19.3, 20.3,
-		21.3, 22.3, 23.3, 24.3, 25.3 , 26.3, 27.3, 28.3, 29.3, 30.3
-	};
+	int ptr = 0;
+	int i = 0, j = 0;
+	int result[30];
 
-	for (int i = 0; i < 3; i++)
+	for (int num = 2; num <= 100; num++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 2; j < num; j++)
 		{
-			printf(" %.1f", num[j + (i * 10)]);
-			printf("\t");
-	
+			if (num % j == 0)
+				break;
 		}
-		printf("\n");
+
+		if (num == j)
+		{
+			result[ptr] = num;
+			printf("%d ", result[ptr]);
+			ptr++;
+		}
 	}
 }
 
